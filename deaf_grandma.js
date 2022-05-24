dd4e04d2cae7e620a3c2f0d65d8d5061677b1aa1
nhungbi <nhungvan16@gmail.com>
2022-05-23 20:02:46 -0500

    fn both

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,27 @@
 function deafGrandma() {
+    let count_bye = 0;
+    console.log('HEY, KID!')
+
+    while (count_bye < 2) {
+
+        let kid_rep = window.prompt('Input a reply to grandma.')
+        if (kid_rep == "") {
+            console.log('WHAT?!')
+        } else if (kid_rep == 'GOODBYE!') {
+            count_bye += 1
+            if (count_bye === 2) {
+                console.log('LATER, SKATER!')
+            } else {
+                console.log('LEAVING SO SOON?')
+            }
+        } else if (Character.isUpperCase(kid_rep)) {
+            consolve.log('NO, NOT SINCE 1946!')
+        } else if (Character.isLpperCase(kid_rep)) {
+            consolve.log('SPEAK UP KID!')
+        }
+    }
 
 }
+
 
 deafGrandma();
--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,4 +1,26 @@
 def deaf_grandma():
+    count_bye = 0
+    print('HEY KID!')
+    while count_bye < 2:
+
+        kid_rep = input('Input a reply to grandma.')
+        if kid_rep == "":
+            print ('WHAT?!')
+            
+        elif kid_rep == 'GOODBYE!':
+            count_bye +=1
+            if count_bye == 2:
+                print('LATER, SKATER!')
+            else:
+                print ('LEAVING SO SOON?')
+
+        elif kid_rep.isupper():
+            print('NO, NOT SINCE 1946!')
+        elif kid_rep.islower():
+            print ("SPEAK UP, KID!")
+        
+
+
 
 
 
